de753dae6817c4f61ef60bb0b0de21ecd9b5ca48
Ernie Yang <ernieyang09@hotmail.com>
2017-08-10 16:00:25 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: `${__dirname}/src/index.html`,
+  filename: 'index.html',
+  inject: 'body',
+});
+
+module.exports = {
+  devtool: "source-map",
+  entry: {
+    index: './src/index.js',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        loader:'babel-loader',
+        test: /\.jsx?/,
+        include: path.resolve(__dirname, 'src')
+      }, {
+        test: /\.html$/,
+        loader: "raw-loader" // loaders: ['raw-loader']，這個方式也是可以被接受的。
+      }
+    ]
+  },
+  devServer:{
+    port:3035,
+    publicPath: "/",
+    contentBase:'./src',
+    inline:true,
+  },
+  plugins:[
+    HTMLWebpackPluginConfig,
+  ]
+}
